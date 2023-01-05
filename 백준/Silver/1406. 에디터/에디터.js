53dabccd00702db030a594d55775005fa2cbaf93
loco9939 <98298711+loco9939@users.noreply.github.com>
2023-01-05 13:13:43 +0900

    [Silver II] Title: 에디터, Time: 860 ms, Memory: 138916 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.js
@@ -0,0 +1,31 @@
+const arr = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const str = [...arr.shift()];
+const N = +arr.shift();
+const command = arr.map((v) => v.split(' '));
+const stack = [];
+
+for (let i = 0; i < N; i++) {
+  if (command[i][0] === 'P') {
+    str.push(command[i][1]);
+  } else if (command[i][0] === 'L') {
+    if (str.length !== 0) {
+      stack.push(str.pop());
+    }
+  } else if (command[i][0] === 'D') {
+    if (stack.length !== 0) {
+      str.push(stack.pop());
+    }
+  } else if (command[i][0] === 'B') {
+    if (str.length !== 0) {
+      str.pop();
+    }
+  }
+}
+
+let length = stack.length;
+for (let i = 0; i < length; i++) {
+  str.push(stack.pop());
+}
+console.log(str.join(''));
+
