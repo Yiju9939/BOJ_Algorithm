5e5db9bfac17958a4d42a05c131848f6cdf1423f
loco9939 <98298711+loco9939@users.noreply.github.com>
2023-01-06 11:22:42 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 2740 ms, Memory: 18088 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
@@ -4,14 +4,13 @@ const N = input.shift();
 const K = input[0];
 
 const arr = Array.from({length:N}, (_,i) => i+1);
-const queue1 = [];
+const result = [];
 
-let pointer = 0
 for (let i = 1; i <= N; i++) {
-    for (let j = pointer; j < K - 1; j++) {
+    for (let j = 0; j < K - 1; j++) {
         arr.push(arr.shift());
     }
-    queue1.push(arr.shift());
+    result.push(arr.shift());
 }
 
-console.log(`<${queue1.join(', ')}>`)
+console.log(`<${result.join(', ')}>`)
