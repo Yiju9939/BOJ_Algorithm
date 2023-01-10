1c4dda12eb15b55d360f57c8e38a031074901cf3
loco9939 <98298711+loco9939@users.noreply.github.com>
2023-01-10 15:27:50 +0900

    [Gold IV] Title: 오큰수, Time: 992 ms, Memory: 215984 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.js
+++ 백준/Gold/17298. 오큰수/오큰수.js
@@ -2,15 +2,13 @@ const input = require("fs").readFileSync("/dev/stdin").toString().trim().split(/
 const N = +input.shift();
 const arr = input.map(Number)
 
-const res = Array.from({ length: N }, () => -1);
-const stack = [];
-
-for (let i = 0; i < N; i++) {
-  while (stack.length > 0 && stack[stack.length - 1][0] < arr[i]) {
-    const [value, idx] = stack.pop();
-    res[idx] = arr[i];
-  }
-  stack.push([arr[i], i]);
+let answer = Array.from({length:N}, () => -1);
+let stack = [0];
+for(let i=1; i<N; i++) {
+    while(arr[stack[stack.length-1]] < arr[i] && stack.length) {
+        answer[stack.pop()] = arr[i];
+    }
+    stack.push(i);
 }
 
-console.log(res.join(' '));
+console.log(answer.join(" "));
