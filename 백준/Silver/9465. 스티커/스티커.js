bc08c8a317d3cb13f8ebb4706b9054a3e194ae8d
loco9939 <98298711+loco9939@users.noreply.github.com>
2023-02-09 15:44:24 +0900

    [Silver I] Title: 스티커, Time: 820 ms, Memory: 120160 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.js
+++ 백준/Silver/9465. 스티커/스티커.js
@@ -5,8 +5,6 @@ function sticker(n, arr) {
   
   DP[0][1] = arr[0][0];
   DP[1][1] = arr[1][0];
-  // DP[0][2] = arr[1][0] + arr[0][1];
-  // DP[1][2] = arr[0][0] + arr[1][1];
   for (let i = 2; i <= n; i++) {
     DP[0][i] = Math.max(DP[1][i - 1], DP[1][i - 2]) + +arr[0][i - 1];
     DP[1][i] = Math.max(DP[0][i - 1], DP[0][i - 2]) + +arr[1][i - 1];
