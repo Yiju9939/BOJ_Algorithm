abda99976eced3513a47202b6da935b9c3456979
loco9939 <98298711+loco9939@users.noreply.github.com>
2023-02-23 11:29:27 +0900

    [Silver II] Title: 사탕 게임, Time: 1948 ms, Memory: 13612 KB -BaekjoonHub

---
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.js
@@ -0,0 +1,51 @@
+const [n,...input] = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
+const N = +n;
+let candy = input.map(v=>v.split(''))
+
+let max = 1;
+
+for(let i = 0; i<N; i++){
+  if(max==N)break;
+  for(let j = 0; j<N; j++){
+    if(max==N)break;
+    candySwap(i,j)
+  }
+}
+console.log(max)
+
+function candySwap(i,j){
+  const dir = [[0,1],[1,0]];
+  dir.forEach(v=>{
+    const [x,y] = v;
+
+    if(i+x>-1 && j+y>-1 && i+x<N && j+y<N && candy[i+x][j+y]!=candy[i][j]){
+        let temp = candy[i][j]
+        candy[i][j] = candy[i+x][j+y]
+        candy[i+x][j+y] = temp
+        checkRow()
+        checkColumn()    
+        candy[i+x][j+y] = candy[i][j]
+        candy[i][j] = temp
+    }
+  })
+}
+
+function checkRow(){
+  for(let i = 0; i<N; i++){
+    let checkArr = [1]
+    for(let j = 1; j<N; j++){
+      checkArr[j] = candy[i][j-1]==candy[i][j] ? checkArr[j-1]+1 :  1;
+    }
+    max = Math.max(...checkArr,max)
+  }
+}
+
+function checkColumn(){
+  for(let i = 0; i<N; i++){
+    let checkArr = [1]
+    for(let j = 1; j<N; j++){
+      checkArr[j] = candy[j-1][i]==candy[j][i] ? checkArr[j-1]+1 : 1;
+    }
+    max = Math.max(...checkArr,max)
+  }
+}
